0443cffd43622f640bdb231fd5c3d9bd4c1aab0b
Matthew Eggers <meggers@cmdagency.com>
2016-02-17 13:12:16 -0800

    Initial commit of the PMF Framework Tutorial

---
+++ html/dontgohere.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "got here";
+
---
+++ html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title></title>
+  </head>
+  <body>
+    <pre>
+      <?php
+        print_r($_GET);
+        print_r($_SERVER);
+      ?>
+    </pre>
+  </body>
+</html>
